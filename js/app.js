5e5277adfa7ab90a3da9d488cde4ed3aa39f43c9
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-06-11 21:07:59 -0400

    added responsive sizing for containers

--- js/app.js
+++ js/app.js
@@ -1,49 +1,25 @@
-function calculateHeight(arr) {
+function calculateDimensions(arr) {
   let ySpacing = 100;
-  let nodeData = [];
-  let cy = 100;
 
-  nodeData.push({"cy": cy})
-  let i = 1;
-
-  while (i < arr.length) {
-
-    let row = Math.ceil(Math.log2(i + 2)) - 1;
-
-    if (i == leftChild(parent(i))) {
-        node = { "cy": nodeData[parent(i)].cy + ySpacing }
-    }
-    else if (i == rightChild(parent(i))) {
-        node = { "cy": nodeData[parent(i)].cy + ySpacing }
-    }
-    nodeData.push(node);
-    ++i;
-  }
-  console.log(nodeData[arr.length - 1].cy)
-  return nodeData[arr.length - 1].cy;
+  depth = Math.ceil(Math.log2((arr.length - 1) + 2)) - 1;
+  // console.log(Math.ceil(depth )
+  return { width: Math.pow(2, depth), height: ySpacing += ySpacing * depth }
 }
 
+let depth;
 
-let testArr = [3,4,2,9,8,7,12];
-makeHeap(testArr, testArr.length);
-let height = calculateHeight(testArr);
+function createContainer(id, arr, width, height) {
+  let box = calculateDimensions(arr);
 
-var svgContainer;
-var arrayContainer;
+  depth = Math.ceil(Math.log2((arr.length - 1) + 2)) - 1 || 1;
 
-svgContainer = d3.select("div#binary-tree")
-                      .append("svg")
-                        .attr("width", 1000)
-                        .attr("height", height);
-
-arrayContainer = d3.select("div#array-visual")
-                      .append("svg")
-                      .attr("width", 800)
-                      .attr("height", 500);
-
-let start = svgContainer.attr("width") / 2;
-
-// console.log(svgContainer.attr("width"));
+  container = d3.select(`div#${id}`)
+                .append("svg")
+                .attr("width", width || box.width * 600 * (.8/depth) * .75)
+                .attr("height", height || box.height)
+  console.log(id, container.attr("width"))
+  return container;
+}
 
 document.getElementById('array').onsubmit = function(evt) {
   evt.preventDefault();
@@ -52,32 +28,24 @@ document.getElementById('array').onsubmit = function(evt) {
 
   if (evt.target.array.value !== '') {
     let arr = evt.target.array.value.trim().split(/\s+|\,+/g).map(function(n) { return parseInt(n)})
-    let height = calculateHeight(arr);
-    svgContainer = d3.select("div#binary-tree")
-                      .append("svg")
-                        .attr("width", 1000)
-                        .attr("height", height);
-    let width = (arr.length * 55);
-    console.log("width", width)
-    arrayContainer = d3.select("div#array-visual")
-                        .append("svg")
-                        .attr("width", width)
-                        .attr("height", 500);
-
     makeHeap(arr, arr.length);
+    svgContainer = createContainer("binary-tree", arr);
+    arrayContainer = createContainer("array-visual", arr, arr.length * 55, 300);
+    start = svgContainer.attr("width") / 2;
     createNodes(arr, start, 50, 35);
     createArray(arr, 0, 30, 50, 50);
   }
 }
 
+let testArr = [3,4,2,9,8,7,12];
+makeHeap(testArr, testArr.length);
 
+var svgContainer = createContainer("binary-tree", testArr)
+var arrayContainer = createContainer("array-visual", testArr, testArr.length * 55, 300);
+let start = svgContainer.attr("width") / 2;
 
-//For Testing
-// let testArr = [3,4,2,9,8,7,12, 6, 13, 25, 86, 56, 64, 10];
-// let testArr = [3,4,2,9,8,7,12];
-// makeHeap(testArr, testArr.length);
 var nodes = createNodes(testArr, start, 50, 35);
-var elements = createArray(testArr, 200, 30, 50, 50);
+var elements = createArray(testArr, 0, 30, 50, 50);
 
 console.log('nodeData in main after creation', nodes)
 
