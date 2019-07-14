ad84143b95eb31e8337df13ce9d80566faba94b1
xxxkang <25734926+xxxkang@users.noreply.github.com>
2019-07-13 20:40:29 -0500

    Changed logic for binary tree deserialization and adjusted horizontal spacing of tree nodes.

--- js/app.js
+++ js/app.js
@@ -10,8 +10,15 @@ function treeAndArray() {
   document.querySelector('#visual-title').innerHTML = "Binary Tree And Array";
   document.querySelector('#instructions').innerHTML = "Click a value in the binary tree or array to highlight its corresponding location in the data structure.";
   if (inputText.value !== '') {
-      input = inputText.value.trim().split(/\s+|\,+/g).map((num) => parseInt(num));
-      createBinaryTreeAndArr(input)
+    // Change how input is handled
+    // input = inputText.value.trim().split('/\s+|\,+/g').map((num) => parseInt(num));
+    // Non-numeric input elements will be converted to NaN
+    input = inputText.value.replace(/'|"|\[|\]/g, '').trim().split(',').map((num) => parseInt(num));
+    // For non-empty input arrays, its length should be odd to make the tree serialization code work
+    if (input.length > 0 && (input.length % 2 == 0)) {
+      input.push(NaN)
+    }
+    createBinaryTreeAndArr(input)
   }
 }
 
--- js/nodes.js
+++ js/nodes.js
@@ -10,170 +10,182 @@ let start;
 
 const xSpacing = 200;
 const ySpacing = 100;
-const radius = 35;
+const radius = 20;
 
-function Node(value, index, depth, cx, cy) {
-  this.value = value;
-  this.index = index;
-  this.depth = depth;
-  this.radius = radius;
-  this.cx = cx;
-  this.cy = cy;
-  this.left = null;
-  this.right = null;
-  this.fill = regFill;
-  this.highlighted = false;
+class Node {
+  constructor(value, index, depth, cx, cy) {
+    this.value = value;
+    this.index = index;
+    this.depth = depth;
+    this.radius = radius;
+    this.cx = cx;
+    this.cy = cy;
+    this.left = null;
+    this.right = null;
+    this.fill = regFill;
+    this.highlighted = false;
+  }
 }
 
-function Tree() {
-  this.nodes = [];
-  this.data = [];
-  this.text = [];
-
-  this.addNode = function(node) {
-    this.data.push(node);
-    this.text = treeContainer.selectAll("text.circle")
-    .data(this.data)
-    .enter()
-    .append("text")
-    .attr("class", "circle")
-    .attr("x", d => d.cx - (d.value.toString().length*4))
-    .attr("y", 0)
-    .text(d => d.value)
-    .transition()
-    .duration(100)
-    .attr("y", d => d.cy + 5)
-    .call(textAttr, regFillText, "sans-serif", "1em")
-
-    this.nodes = treeContainer.selectAll("circle")
-                .data(this.data)
-                .enter()
-                .append("circle")
-  }
-
-  this.updateNodes = function() {
-    this.nodes = treeContainer.selectAll("circle")
-    .data(this.data)
-    .enter()
-    .append("circle")
-  }
-
-  this.swapNodeData = function(a, b) {
-    let temp = this.data[a];
-    this.data[a] = this.data[b];
-    this.data[b] = temp;
-  }
-
-  this.findNode = function(index) {
-    return this.nodes.filter((d) => d.index === index)
-  }
-
-  this.findText = function(index) {
-    return this.text.filter((d) => d.index === index)
-  }
-
-  this.removeNode = function(index) {
-    this.findNode(index).remove();
-    this.findText(index).remove();
-
-    this.data = this.data.filter((e, i) => { return i !== index})
-    this.text = this.text.filter((e, i) => { return i !== index})
-
-    this.nodes = treeContainer.selectAll("circle")
-            .data(this.data)
-            .exit().remove()
-  }
-
-  this.createBinaryTree = function(arr) {
-    treeContainer = createContainer("binary-tree", arr);
-    start = treeContainer.attr("width") / 2;
-
-    let i = 0;
-    let node = {};
-
-    while (i < arr.length) {
-      let depth = Math.ceil(Math.log2(i + 2)) - 1;
-
-      node = new Node(arr[i], i, depth);
-
-      if (i === 0) {
+class Tree {
+  constructor() {
+    this.nodes = [];
+    this.data = [];
+    this.text = [];
+    this.addNode = function (node) {
+      this.data.push(node);
+      this.text = treeContainer.selectAll("text.circle")
+        .data(this.data)
+        .enter()
+        .append("text")
+        .attr("class", "circle")
+        .attr("x", d => d.cx - (d.value.toString().length * 4))
+        .attr("y", 0)
+        .text(d => d.value)
+        .transition()
+        .duration(100)
+        .attr("y", d => d.cy + 5)
+        .call(textAttr, regFillText, "sans-serif", "1em");
+      this.nodes = treeContainer.selectAll("circle")
+        .data(this.data)
+        .enter()
+        .append("circle");
+    };
+    this.updateNodes = function () {
+      this.nodes = treeContainer.selectAll("circle")
+        .data(this.data)
+        .enter()
+        .append("circle");
+    };
+    this.swapNodeData = function (a, b) {
+      let temp = this.data[a];
+      this.data[a] = this.data[b];
+      this.data[b] = temp;
+    };
+    this.findNode = function (index) {
+      return this.nodes.filter((d) => d.index === index);
+    };
+    this.findText = function (index) {
+      return this.text.filter((d) => d.index === index);
+    };
+    this.removeNode = function (index) {
+      this.findNode(index).remove();
+      this.findText(index).remove();
+      this.data = this.data.filter((e, i) => { return i !== index; });
+      this.text = this.text.filter((e, i) => { return i !== index; });
+      this.nodes = treeContainer.selectAll("circle")
+        .data(this.data)
+        .exit().remove();
+    };
+    this.createBinaryTree = function (arr) {
+      treeContainer = createContainer("binary-tree", arr);
+      start = treeContainer.attr("width") / 2;
+      let containerWidth = treeContainer.attr("width");
+      let i = 1;
+      let queue = [];
+      let node = null;
+      // Create the first node, the root node, if the arr is non-empty
+      if (arr.length > 0) {
+        node = new Node(arr[0], 0, 0);
         node.cx = start;
         node.cy = radius;
+        this.addNode(node);
+        queue.push([node, 0]); // node value, depth
       }
-      else {
-        if (i == leftChild(parent(i))) {
-          node.cx = this.data[parent(i)].cx - xSpacing/depth;
+      while (i < arr.length) {
+        // let depth = Math.ceil(Math.log2(i + 2)) - 1;
+        let next = queue.shift();
+        let curNode = next[0];
+        let curDepth = next[1];
+        let leftValue = arr[i];
+        let rightValue = arr[i + 1];
+        // Left
+        if (isNaN(leftValue)) {
+          curNode.left = null;
         }
         else {
-          node.cx = this.data[parent(i)].cx + xSpacing/depth;
+          node = new Node(leftValue, i, curDepth + 1);
+          curNode.left = node;
+          // node coordinates
+          node.cx = curNode.cx - containerWidth / Math.pow(2, curDepth+2);
+          node.cy = curNode.cy + ySpacing;
+          // add a line from its parent to the node
+          treeContainer.append("line").call(createLineAttr, "black", curNode.cx, curNode.cy, node.cx, node.cy);
+          this.addNode(node);
+          // append to the queue of nodes
+          queue.push([node, curDepth + 1]);
         }
-        node.cy = this.data[parent(i)].cy + ySpacing;
-        treeContainer.append("line").call(createLineAttr, "black", this.data[parent(i)].cx, this.data[parent(i)].cy, node.cx, node.cy);
+        // Right
+        if (isNaN(rightValue)) {
+          curNode.right = null;
+        }
+        else {
+          node = new Node(rightValue, i + 1, curDepth + 1);
+          curNode.right = node;
+          // node coordinates
+          node.cx = curNode.cx + containerWidth / Math.pow(2, curDepth+2);
+          node.cy = curNode.cy + ySpacing;
+          // add a line from its parent to the node
+          treeContainer.append("line").call(createLineAttr, "black", curNode.cx, curNode.cy, node.cx, node.cy);
+          this.addNode(node);
+          // append to the queue of nodes
+          queue.push([node, curDepth + 1]);
+        }
+        // Get two nodes at each loop
+        i += 2;
       }
-      this.addNode(node);
-      ++i;
-    }
-    this.nodes = treeContainer
-                  .selectAll("circle")
-                  .raise()
-                  .on("click", addHighlight)
-    this.text = treeContainer
-                .selectAll("text.circle")
-                .raise()
-                .on("click", addHighlight)
-    this.nodes.call(circleAttr);
-  }
-
-  this.createBinarySearchTree = function(inputArr) {
-    treeContainer = createContainer("binary-tree", inputArr);
-    start = treeContainer.attr("width") / 2;
-
-    let midPoint = Math.floor(inputArr.length / 2);
-    let root = new Node(inputArr[midPoint], null, 1, start, radius);
-
-    const insertNode = (arr, depth, cx) => {
-      if (!arr.length) { return; }
-      let mid = Math.floor(arr.length / 2);
-      let node = new Node(arr[mid], null, depth, cx , radius + (depth * ySpacing));
-      let nextDepth = depth + 1;
-
-      node.left = insertNode(arr.slice(0, mid), nextDepth, cx - xSpacing/nextDepth);
-      node.right = insertNode(arr.slice(mid + 1), nextDepth, cx + xSpacing/nextDepth);
-
-      if (arr.slice(0, mid).length) {
-      treeContainer.append("line").call(createLineAttr, "black", cx, radius+(depth * ySpacing), cx - xSpacing/nextDepth, radius + nextDepth * ySpacing);
+      this.nodes = treeContainer
+        .selectAll("circle")
+        .raise()
+        .on("click", addHighlight);
+      this.text = treeContainer
+        .selectAll("text.circle")
+        .raise()
+        .on("click", addHighlight);
+      this.nodes.call(circleAttr);
+    };
+    this.createBinarySearchTree = function (inputArr) {
+      treeContainer = createContainer("binary-tree", inputArr);
+      start = treeContainer.attr("width") / 2;
+      let midPoint = Math.floor(inputArr.length / 2);
+      let root = new Node(inputArr[midPoint], null, 1, start, radius);
+      const insertNode = (arr, depth, cx) => {
+        if (!arr.length) {
+          return;
+        }
+        let mid = Math.floor(arr.length / 2);
+        let node = new Node(arr[mid], null, depth, cx, radius + (depth * ySpacing));
+        let nextDepth = depth + 1;
+        node.left = insertNode(arr.slice(0, mid), nextDepth, cx - xSpacing / nextDepth);
+        node.right = insertNode(arr.slice(mid + 1), nextDepth, cx + xSpacing / nextDepth);
+        if (arr.slice(0, mid).length) {
+          treeContainer.append("line").call(createLineAttr, "black", cx, radius + (depth * ySpacing), cx - xSpacing / nextDepth, radius + nextDepth * ySpacing);
+        }
+        if (arr.slice(mid + 1).length) {
+          treeContainer.append("line").call(createLineAttr, "black", cx, radius + (depth * ySpacing), cx + xSpacing / nextDepth, radius + nextDepth * ySpacing);
+        }
+        this.addNode(node);
+      };
+      root.left = insertNode(inputArr.slice(0, midPoint), 1, start - xSpacing);
+      root.right = insertNode(inputArr.slice(midPoint + 1), 1, start + xSpacing);
+      if (inputArr.slice(0, midPoint).length) {
+        treeContainer.append("line").call(createLineAttr, "black", start, radius, start - xSpacing, radius + ySpacing);
       }
-      if (arr.slice(mid + 1).length) {
-      treeContainer.append("line").call(createLineAttr, "black", cx, radius+(depth * ySpacing), cx + xSpacing/nextDepth, radius + nextDepth * ySpacing);
+      if (inputArr.slice(midPoint + 1).length) {
+        treeContainer.append("line").call(createLineAttr, "black", start, radius, start + xSpacing, radius + ySpacing);
       }
-
-      this.addNode(node)
-    }
-
-    root.left = insertNode(inputArr.slice(0, midPoint), 1, start - xSpacing);
-    root.right = insertNode(inputArr.slice(midPoint + 1), 1, start + xSpacing);
-
-    if (inputArr.slice(0, midPoint).length) {
-      treeContainer.append("line").call(createLineAttr, "black", start, radius, start - xSpacing, radius + ySpacing);
-    }
-    if (inputArr.slice(midPoint + 1).length) {
-    treeContainer.append("line").call(createLineAttr, "black", start, radius, start + xSpacing, radius + ySpacing);
-    }
-    this.addNode(root)
-
-    this.nodes = treeContainer
-    .selectAll("circle")
-    .raise()
-
-    this.text = treeContainer
-    .selectAll("text.circle")
-    .raise()
-
-    this.nodes.call(circleAttr);
-  }
-
-  this.size = function() {
-    return d3.selectAll("circle").nodes().length;
+      this.addNode(root);
+      this.nodes = treeContainer
+        .selectAll("circle")
+        .raise();
+      this.text = treeContainer
+        .selectAll("text.circle")
+        .raise();
+      this.nodes.call(circleAttr);
+    };
+    this.size = function () {
+      return d3.selectAll("circle").nodes().length;
+    };
   }
 }
 
@@ -280,18 +292,21 @@ function removeHighlight() {
 }
 
 function calcDimensions(arr) {
-  let depth = Math.ceil(Math.log2((arr.length - 1) + 2)) - 1;
-  return { width: Math.pow(2, depth), height: ySpacing + ySpacing * depth, depth: depth }
+  let maxDepth = arr.length / 2;
+  return { width: arr.length,
+    height: ySpacing + ySpacing * maxDepth,
+    depth: maxDepth,
+  };
 }
 
 function createContainer(id, arr, width, height) {
   let box = calcDimensions(arr);
 
-  let depth = Math.ceil(Math.log2((arr.length - 1) + 2)) - 1 || 1;
+  let depth = box.depth || 1;
 
   let container = d3.select(`div#${id}`)
     .append('svg')
-    .attr('width', width || box.width * 600 * (.8 / depth) * .75)
+    .attr('width', width || box.width * 80)
     .attr('height', height || box.height)
 
   return container;
